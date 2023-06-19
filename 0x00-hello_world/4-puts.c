2686b0892a8e25fdcdefd63ca6a43d88cb17b668
Yousef <yousef1@gmail.com>
2023-06-19 15:29:22 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bah
 #include<stdio.h>
 /**
  * main - Entry point
@@ -10,4 +10,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
 
