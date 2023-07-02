bef786e4a80ff20b39760b2771d6bf5a56c492ff
Yousef <yousef1@gmail.com>
2023-07-02 12:44:38 -0700

    my 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
-/*
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- * */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("pProgramming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
 
