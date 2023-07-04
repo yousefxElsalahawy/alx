b22533e05c0d78bcf4f33a3a2650816201076418
Yousef <yousef1@gmail.com>
2023-07-04 02:51:09 -0700

    my 2 commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	for (int i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+}
+
