68f42263863a9fed8adfa9de5004a616014684a7
Yousef <yousef1@gmail.com>
2023-07-23 13:45:29 -0700

    my new commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,31 @@
 #include "main.h"
+
 /**
- * main - check the code
- * @s1: the first variable
- * @s2: the second variable
- *
- * Return: Always 0.
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
 	else if (*s1 == *s2)
+	{
 		return (wildcmp(s1 + 1, s2 + 1));
-	else
-		return (0);
+	}
+	return (0);
 }
+
