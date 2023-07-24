bef223ee2110298c47e6a283131518ebb6675a21
Yousef <yousef1@gmail.com>
2023-07-24 09:11:33 -0700

    my 1 commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * main - Write a program that prints the number of arguments passed into it
- * @argc: number of argument
- * @argv: array of argument
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-int main(int argc, int *argv[])
+
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%d\n", argv[0]);
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
+
+
