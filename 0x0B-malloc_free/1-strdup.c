8c13ce4d82eff336e4a8bace5506d58c8146b475
You <you@gmail.com>
2023-07-31 22:56:10 +0300

    my commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+		
+#include <stdlib.h>
+		
+#include "main.h"
+		
+/**
+		
+ * _strdup - duplicate to new memory space location
+		
+ * @str: char
+		
+ * Return: 0
+		
+ */
+		
+char *_strdup(char *str)
+		
+{
+		
+	char *aaa;
+		
+	int i, r = 0;
+		
+
+		
+	if (str == NULL)
+		
+		return (NULL);
+		
+	i = 0;
+		
+	while (str[i] != '\0')
+		
+
