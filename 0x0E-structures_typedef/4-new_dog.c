978b5cb89c8a596d76562c3408a1ba5709fd6610
You <you@gmail.com>
2023-07-31 21:57:10 +0300

    my commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -61,15 +61,47 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int len1, len2;
+    dog_t *dog;
+    int len1, len2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+    len1 = _strlen(name);
+    len2 = _strlen(owner);
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
+    dog = malloc(sizeof(dog_t));
+    if (dog == NULL)
+        return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
+    dog->name = malloc(sizeof(char) * (len1 + 1));
+    if (dog->name == NULL)
+    {
+        free(dog);
+        return (NULL);
+    }
 
+    dog->owner = malloc(sizeof(char) * (len2 + 1));
+    if (dog->owner == NULL)
+    {
+        free(dog->name);
+        free(dog);
+        return (NULL);
+    }
+
+    if (_strcpy(dog->name, name) == NULL)
+    {
+        free(dog->name);
+        free(dog->owner);
+        free(dog);
+        return (NULL);
+    }
+
+    if (_strcpy(dog->owner, owner) == NULL)
+    {
+        free(dog->owner);
+        free(dog);
+        return (NULL);
+    }
+
+    dog->age = age;
+
+    return (dog);
+}
