f6ca6361e172ad646401d60999d0a6a24f35e081
yousefxElsalahawy <yousefxElsalahawy@gmail.com>
2023-08-06 08:15:55 -0700

    my 1 commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Write a function that checks for uppercase character.
+ *
+ * @c: char to check
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
