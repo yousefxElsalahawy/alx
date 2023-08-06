e6f4d7b75ceccfbbf47153995f71824dcd30e1ef
yousefxElsalahawy <yousefxElsalahawy@gmail.com>
2023-08-06 09:10:55 -0700

    my 1 commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,8 @@ void print_most_numbers(void)
 {
 	for (char i = '0'; i <= '9'; i++)
 	{
-		if (i == '2' || i == '4')
-			countinue;
-		else
+		if (i != '2' || i != '4')
 			putchar(i);
 	}
+	putchar('\n');
 }
