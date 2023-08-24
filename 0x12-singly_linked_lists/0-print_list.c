cea51c59cb7aee26302ff3888fabb49ab013e1a8
yousef <yousef@gmail.com>
2023-08-24 03:38:07 -0700

    my 1 commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include <list.h>
+
+/**
+ * strlen - return the lenght of string
+ *@s: string 
+ *
+ * Return : the length of string
+ */
+
+int strlen (char *s)
+{
+	while(*s)
+		return (1 + strlen(s + 1));
+}
+
+/**
+ * print_list - singly linked list
+ * @h: node 
+ *
+ * Return : the number of node 
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	if (h)
+	{
+		printf("[%d] %s\n" , strlen(h->str), h->str ? h->str : "(nil)")
+		i++;
+		h = h->next;
+	}
+	return (i);
+
+}
+
