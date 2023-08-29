5acffa9d1952f60e9f719ba0ea89b6fe45a0b389
yousef <yousef@gmail.com>
2023-08-29 03:38:26 -0700

    my 1 commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 	new->next = NULL;
-	if (idx == 1)
+	if (idx == 0)
 	{
 		new->next = current;
 		current = new;
