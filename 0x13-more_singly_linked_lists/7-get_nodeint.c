ba4c846c031326a432fd861506aee425cfdfa80f
yousef <yousef@gmail.com>
2023-08-29 00:00:56 -0700

    my 1 commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,18 +7,24 @@
  *
  * Return: pointer to the node we're looking for, or NULL
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+istint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp = head;
 
-	while (temp && i < index)
+	if (!head)
+		return (NULL);
+
+	while (head)
 	{
-		temp = temp->next;
-		i++;
+		if (i <  index)
+		{
+			i++;
+			head = head->next;
+		}
+		else
+			return (head);
+			break;
 	}
+	return (NULL);
 
-	return (temp ? temp : NULL);
 }
-
-
