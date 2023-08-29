980dcd7ab70bba20001b364d120af84236c37566
yousef <yousef@gmail.com>
2023-08-29 03:42:39 -0700

    my 1 commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,38 +11,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	listint_t *new;
-	listint_t *current = *head;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-
-	if (new == NULL || current == NULL)
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
+
 	if (idx == 0)
 	{
-		new->next = current;
-		current = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 
-	while (current)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (i < idx - 1)
+		if (i == idx - 1)
 		{
-			current = current->next;
-			i++;
-		}
-		else
-		{
-			new->next = current->next;
-			current->next = new;
-
+			new->next = temp->next;
+			temp->next = new;
 			return (new);
 		}
+		else
+			temp = temp->next;
 	}
+
 	return (NULL);
 }
+
+
+
+
